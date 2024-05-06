653580653b3dd99b21d04bcceb6e1a516d38111d
nguyen.nd <nguyen.nd@josys.com>
2024-05-06 14:44:07 +0700

    first commit

---
+++ src/components/particles/DataLists.tsx
@@ -0,0 +1,229 @@
+export const NavLinks = [
+  {
+    name: 'Địa điểm',
+    url: '/',
+  },
+  {
+    name: 'Đặt chỗ',
+    url: '/',
+  },
+]
+
+export const NavButtons = [
+  {
+    name: 'Đăng nhập',
+    url: '/',
+  },
+  {
+    name: 'Đăng ký',
+    url: '/',
+  },
+]
+
+export const HeroTexts = {
+  firstText: 'THUẬN THU LIMOUSINE',
+  secondText: 'Khám phá dịch vụ limousine liên tỉnh sang trọng tại Việt Nam',
+  thirdText:
+    'Hành trình đầy lịch lãm và thoải mái đến những điểm đến tuyệt vời',
+  firstButton: 'Tìm hiểu thêm',
+  secondButton: 'Xem demo',
+}
+
+export const ServiceTexts = {
+  firstText: 'DỊCH VỤ',
+  secondText: 'Chúng tôi cung cấp những dịch vụ tốt nhất',
+  cards: [
+    {
+      firstText: 'Dự báo thời tiết',
+      secondText:
+        'Built Wicket longer admire do barton vanity itself do in it.',
+    },
+    {
+      firstText: 'Chuyến bay tốt nhất',
+      secondText: 'Engrossed listening. Park gate sell they west hard for the.',
+    },
+    {
+      firstText: 'Sự kiện địa phương',
+      secondText:
+        'Barton vanity itself do in it. Preferred to men it engrossed listening.',
+    },
+    {
+      firstText: 'Tùy chỉnh',
+      secondText:
+        'We deliver outsourced aviation services for military customers.',
+    },
+  ],
+}
+
+export const TopDestinationTexts = {
+  firstText: 'Bán chạy nhất',
+  secondText: 'Điểm đến hàng đầu',
+  cards: [
+    {
+      country: 'Rome, Italy',
+      price: '5.42k',
+      duration: 'Chuyến đi 10 ngày',
+    },
+    {
+      country: 'London, UK',
+      price: '4.2k',
+      duration: 'Chuyến đi 12 ngày',
+    },
+    {
+      country: 'Paris, France',
+      price: '15k',
+      duration: 'Chuyến đi 26 ngày',
+    },
+    {
+      country: 'New York, USA',
+      price: '4.5k',
+      duration: 'Chuyến đi 8 ngày',
+    },
+    {
+      country: 'Warsaw, Poland',
+      price: '3.9k',
+      duration: 'Chuyến đi 14 ngày',
+    },
+    {
+      country: 'Dubai, UAE',
+      price: '9.2k',
+      duration: 'Chuyến đi 17 ngày',
+    },
+  ],
+}
+
+export const BookingStepsTexts = {
+  firstText: 'Dễ dàng và nhanh chóng',
+  secondText: 'Đặt chuyến đi tiếp theo của bạn trong 3 bước dễ dàng',
+  listOfSteps: [
+    {
+      text: 'Chọn điểm đến lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna, tortor tempus.',
+    },
+    {
+      text: 'Thực hiện thanh toán lorem ipsum dolor sit amet, consectetur adipiscing elit. Uma, tortor tempus.',
+    },
+    {
+      text: 'Đến sân bay vào ngày đã chọn lorem ipsum dolor sit amet, consectetur adipiscing elit. Uma tortor tempus.',
+    },
+  ],
+  cardOne: {
+    name: 'Chuyến đi đến Hy Lạp',
+    date: '14-29 tháng 6 | bởi Robbin Jobs',
+    people: '24 người tham gia',
+  },
+  cardTwo: {
+    status: 'Đang diễn ra',
+    destination: 'Chuyến đi đến Rome',
+    completion: 'Hoàn thành 40%',
+  },
+}
+
+export const TestimonialTexts = {
+  firstText: 'LỜI KHEN NGỢI',
+  secondText: 'Những lời nói của khách hàng về chúng tôi.',
+  feedBacks: [
+    {
+      text: 'Bạn đã làm một công việc tuyệt vời. Không có trục trặc nào. Cảm ơn bạn một lần nữa vì đã lên kế hoạch cho chuyến đi này.',
+      person: 'Mike taylor',
+      location: 'Lahore, Pakistan',
+    },
+    {
+      text: 'Jadoo đã làm tốt trong suốt quá trình từ lập kế hoạch đến cập nhật trong chuyến đi. Chúng tôi có 11 người và mọi thứ được thực hiện hoàn hảo. Chúng tôi đánh giá cao tất cả công sức của cô ấy. Đó thực sự là chuyến đi của đời người. Cảm ơn bạn!',
+      person: 'Gina',
+      location: 'Owerri, Nigeria',
+    },
+    {
+      text: 'Đặt qua bạn rất dễ dàng và làm cho cuộc sống của chúng tôi dễ dàng hơn nhiều. Tôi không có gì để phàn nàn! Cảm ơn bạn đã cho chúng tôi lời khuyên và hướng dẫn trước khi chúng tôi đi về những gì cần mang theo và những thứ tương tự, điều đó rất hữu ích!',
+      person: 'Emma',
+      location: 'Vancouver, Canada',
+    },
+    {
+      text: 'Cảm ơn bạn vì đã giới thiệu và tổ chức chuyến đi này. Khu nghỉ mát thật đẹp. Các hồ bơi vô cực, những cây cọ trong hồ bơi chính, hồ bơi vô cực trong khu vực ưa thích của chúng tôi nhìn ra vịnh và sân golf thật ngoại hạng....',
+      person: 'Loveth',
+      location: 'Lagos, Nigeria',
+    },
+  ],
+}
+
+export const NewsletterTexts = {
+  firstText:
+    'Đăng ký để nhận thông tin, tin tức mới nhất và các ưu đãi thú vị khác về Jadoo.',
+  placeholderText: 'Email của bạn',
+  buttonText: 'Đăng ký',
+}
+
+export const FooterTexts = {
+  underLogoText:
+    'Đặt chuyến đi của bạn trong vài phút, kiểm soát hoàn toàn trong thời gian dài hơn.',
+  quickLinks: {
+    caption: 'Liên kết nhanh',
+    links: [
+      {
+        name: 'Điểm đến',
+        url: '/',
+      },
+      {
+        name: 'Khách sạn',
+        url: '/',
+      },
+      {
+        name: 'Chuyến bay',
+        url: '/',
+      },
+      {
+        name: 'Đặt chỗ',
+        url: '/',
+      },
+      {
+        name: 'Đăng nhập',
+        url: '/',
+      },
+      {
+        name: 'Đăng ký',
+        url: '/',
+      },
+    ],
+  },
+  contacts: {
+    caption: 'Liên hệ',
+    links: [
+      {
+        name: 'Câu hỏi thường gặp',
+        url: '/',
+      },
+      {
+        name: 'Trợ giúp',
+        url: '/',
+      },
+      {
+        name: 'Chính sách',
+        url: '/',
+      },
+      {
+        name: 'Điều khoản & Điều kiện',
+        url: '/',
+      },
+    ],
+  },
+  more: {
+    caption: 'Thêm',
+    links: [
+      {
+        name: 'Nghề nghiệp',
+        url: '/',
+      },
+      {
+        name: 'Hãng hàng không',
+        url: '/',
+      },
+      {
+        name: 'Phí hàng không',
+        url: '/',
+      },
+      {
+        name: 'Mẹo giá rẻ',
+        url: '/',
+      },
+    ],
+  },
+}
